0ffeb62b949a9b1f893ca0c35c134953ee412e77
kashiinadeem <53114740+kashiinadeem@users.noreply.github.com>
2024-07-10 17:31:09 +0500

    customized add new issue page

---
+++ app/issues/new/page.tsx
@@ -0,0 +1,16 @@
+'use client'
+import { Button, TextField } from '@radix-ui/themes'
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+
+const NewIssuePage = () => {
+    return (
+        <div className="max-w-xl space-y-2">
+            <TextField.Root placeholder='Title'></TextField.Root>
+            <SimpleMDE placeholder='Description'></SimpleMDE>
+            <Button>Submit New Issue</Button>
+        </div>
+    )
+}
+
+export default NewIssuePage
