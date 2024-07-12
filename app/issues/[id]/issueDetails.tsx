df54cc1e53e871ff5bcd108a1ed316ea0dd39709
kashiinadeem <53114740+kashiinadeem@users.noreply.github.com>
2024-07-12 16:37:19 +0500

    refactor applied single responsibility

---
+++ app/issues/[id]/issueDetails.tsx
@@ -0,0 +1,23 @@
+import { IssueStatusBadge } from '@/app/components'
+import { Issue } from '@prisma/client';
+import { Card, Flex, Heading, Text } from '@radix-ui/themes'
+import ReactMardown from 'react-markdown';
+
+const IssueDetails = ({ issue }: { issue: Issue }) => {
+    return (
+        <>
+            <Heading>{issue.title}</Heading>
+            <Flex gap='4'><IssueStatusBadge status={issue.status} />
+                <Text>{issue.createdAt.toDateString()}</Text></Flex>
+            <Card className='prose' mt='4'>
+                <p>
+                    <ReactMardown>
+                        {issue.description}
+                    </ReactMardown>
+                </p>
+            </Card>
+        </>
+    )
+}
+
+export default IssueDetails
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,17 +1,14 @@
-import IssueStatusBadge from '@/app/components/issueStatusBadge';
 import prisma from '@/prisma/client';
-import { Box, Button, Card, Flex, Grid, Heading, Text } from '@radix-ui/themes';
+import { Box, Grid } from '@radix-ui/themes';
 import { notFound } from 'next/navigation';
-import {Pencil2Icon} from '@radix-ui/react-icons'
-import ReactMardown from 'react-markdown';
-import Link from 'next/link';
-
+import EditIssueButton from './editIssueButton';
+import IssueDetails from './issueDetails';
 
 interface Props {
     params: { id: string }
 }
 
-const IssueDetails = async ({ params }: Props) => {
+const IssueDetailsPage = async ({ params }: Props) => {
 
     const issue = await prisma.issue.findUnique({
         where: {
@@ -25,26 +22,14 @@ const IssueDetails = async ({ params }: Props) => {
     return (
         <Grid columns={{ initial: "1", md: "2"}} gap="5">
             <Box>
-                <Heading>{issue.title}</Heading>
-                <Flex gap='4'><IssueStatusBadge status={issue.status} />
-                    <Text>{issue.createdAt.toDateString()}</Text></Flex>
-                <Card className='prose' mt='4'>
-                    <p>
-                        <ReactMardown>
-                            {issue.description}
-                        </ReactMardown>
-                    </p>
-                </Card>
+                <IssueDetails issue={issue}/>
             </Box>
             <Box>
-                <Button>
-                    <Pencil2Icon />
-                    <Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
-                </Button>
+                <EditIssueButton issueId={issue.id}/>
             </Box>
         </Grid>
 
     )
 }
 
-export default IssueDetails
+export default IssueDetailsPage
