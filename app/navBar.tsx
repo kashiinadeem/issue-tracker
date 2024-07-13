5694fe4830c89c2894803e97f75c0694b8f40699
kashiinadeem <53114740+kashiinadeem@users.noreply.github.com>
2024-07-13 18:23:42 +0500

    add a drop down menu to show the current user

--- app/navBar.tsx
+++ app/navBar.tsx
@@ -1,46 +1,82 @@
-'use client';
-import Link from 'next/link'
-import { usePathname } from 'next/navigation';
-import React from 'react'
-import { AiFillBug } from 'react-icons/ai'
-import { useSession } from 'next-auth/react'
-import { Box, Container, Flex } from '@radix-ui/themes';
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { AiFillBug } from "react-icons/ai";
+import { useSession } from "next-auth/react";
+import {
+  Avatar,
+  Box,
+  Container,
+  DropdownMenu,
+  Flex,
+  Text,
+} from "@radix-ui/themes";
 
 const NavBar = () => {
+  const currentPath = usePathname();
+  const { status, data: session } = useSession();
 
-    const currentPath = usePathname();
-    const { status, data: session } = useSession();
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
 
-    const links = [
-        { label: 'Dashboard', href: '/' },
-        { label: 'Issues', href: '/issues' }
+  return (
+    <nav className="border-b mb-5 px-5 h-14 py-3">
+      <Container>
+        <Flex justify={"between"}>
+          <Flex align="center" gap="3">
+            <Link href="/">
+              <AiFillBug />
+            </Link>
+            <ul className="flex space-x-6">
+              {links.map((link) => (
+                <li key={link.href}>
+                  <Link
+                    className={`${
+                      link.href === currentPath
+                        ? "text-zinc-950"
+                        : "text-zinc-500"
+                    } hover:text-zinc-800 translate-x-5`}
+                    href={link.href}
+                  >
+                    {link.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </Flex>
+          <Box>
+            {status === "authenticated" && (
+              <DropdownMenu.Root>
+                <DropdownMenu.Trigger>
+                  <Avatar
+                    src={session.user!.image!}
+                    fallback="?"
+                    size="2"
+                    radius="full"
+                    className="cursor-pointer"
+                  ></Avatar>
+                </DropdownMenu.Trigger>
+                <DropdownMenu.Content>
+                  <DropdownMenu.Label>
+                    <Text size="2">{session.user?.email}</Text>
+                  </DropdownMenu.Label>
+                  <DropdownMenu.Item>
+                    <Link href="/api/auth/signout">Log Out</Link>
+                  </DropdownMenu.Item>
+                </DropdownMenu.Content>
+              </DropdownMenu.Root>
+            )}
+            {status === "unauthenticated" && (
+              <Link href="/api/auth/signin">Log In</Link>
+            )}
+          </Box>
+        </Flex>
+      </Container>
+    </nav>
+  );
+};
 
-    ]
-
-    return (
-        <nav className='border-b mb-5 px-5 h-14 py-3'>
-            <Container>
-                <Flex justify={'between'}>
-                    <Flex align='center' gap='3'>
-                        <Link href="/"><AiFillBug /></Link>
-                        <ul className='flex space-x-6'>
-                            {links.map(link =>
-                                <li key={link.href}>
-                                    <Link
-                                        className={`${link.href === currentPath ? 'text-zinc-950' : 'text-zinc-500'} hover:text-zinc-800 translate-x-5`}
-                                        href={link.href}>{link.label}</Link></li>)}
-                        </ul>
-                    </Flex>
-                    <Box>
-                        {status === 'authenticated' && <Link href="/api/auth/signout">Log Out</Link>}
-                        {status === 'unauthenticated' && <Link href="/api/auth/signin">Log In</Link>}
-                    </Box>
-                </Flex>
-
-            </Container>
-
-        </nav>
-    )
-}
-
-export default NavBar
+export default NavBar;
